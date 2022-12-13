814abe04b9e2b64dedb8805e25d3a21c4018f851
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2022-12-13 07:05:25 +0000

    Create Disable_Transport_Rules.ps1

---
+++ Exchange Online/Disable_Transport_Rules.ps1
@@ -0,0 +1,16 @@
+#Connect to Exchange Online
+Connect-ExchangeOnline
+
+#View a list of enabled transport rules
+Get-TransportRule | Where-Object {$_.State -eq "Enabled"}
+
+#Define and store your transport rule
+$Rule = Get-TransportRule -Identity "Block .exe attachments"
+
+#Check if enabled and disable the rule
+If ($rule.state -eq "Enabled") {
+   Disable-TransportRule -Identity $Rule.Name -confirm:$false
+    Write-host $rule.name ": has been disabled" -ForegroundColor black -BackgroundColor green
+} Else {
+    Write-host $rule.name ": is already disabled" -ForegroundColor black -BackgroundColor yellow
+}
