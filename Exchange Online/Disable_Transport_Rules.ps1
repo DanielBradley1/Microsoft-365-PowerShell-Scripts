f28bafe457710636ff64055a0e976b03ff6ff52a
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2022-12-13 07:07:19 +0000

    Update Disable_Transport_Rules.ps1

--- Exchange Online/Disable_Transport_Rules.ps1
+++ Exchange Online/Disable_Transport_Rules.ps1
@@ -1,3 +1,19 @@
+<#
+.DESCRIPTION
+Disable a single transport rule in Exchange Online
+.NOTES
+  Version:        1.0.0
+  Author:         Daniel Bradley
+  Twitter:        https://twitter.com/DanielatOCN
+  Website:        https://ourcloudnetwork.com/
+  LinkedIn        https://www.linkedin.com/in/danielbradley2/
+  Creation Date:  13/12/2022
+  Purpose/Change: First draft
+#>
+
+#Import module
+Import-Module ExchangeOnlineManagement
+
 #Connect to Exchange Online
 Connect-ExchangeOnline
 
@@ -5,7 +21,7 @@ Connect-ExchangeOnline
 Get-TransportRule | Where-Object {$_.State -eq "Enabled"}
 
 #Define and store your transport rule
-$Rule = Get-TransportRule -Identity "Block .exe attachments"
+$Rule = Get-TransportRule -Identity "###Block .exe attachments###"
 
 #Check if enabled and disable the rule
 If ($rule.state -eq "Enabled") {
